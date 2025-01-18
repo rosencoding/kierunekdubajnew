9be5eca4ccd630389d603e692c331bc891511d93
rosencoding <krz.cichy@gmail.com>
2025-01-19 00:02:32 +0100

    Update PageLayout with proper routing setup

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const PageLayout = () => {
+  const location = useLocation();
+  
   return (
     <div className="flex flex-col min-h-screen">
       <Helmet>
