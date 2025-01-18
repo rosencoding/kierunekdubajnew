6c6491db940adf42094325f63ac4bc260619c6cf
rosencoding <krz.cichy@gmail.com>
2025-01-18 23:12:36 +0100

    Fix navigation issue: Remove usePageTitle hook from PageLayout to prevent title update conflicts

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -3,11 +3,8 @@ import { Helmet } from 'react-helmet';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import usePageTitle from '../hooks/usePageTitle';
 
 const PageLayout = () => {
-  usePageTitle();
-
   return (
     <div className="flex flex-col min-h-screen">
       <Helmet>
